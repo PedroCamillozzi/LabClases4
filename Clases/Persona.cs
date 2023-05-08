a53e4ef406877b7b86928c4f7e2c9f290caffb2f
PedroCamillozzi <pedrocamillozzi01@gmail.com>
2023-05-08 14:08:20 -0300

    Agregar archivos de proyecto.

---
+++ Clases/Persona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabClases4.Clases
+{
+    internal class Persona
+    {
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+        private int _dni;
+
+        public Persona(string nom, string ape, int edad, int dni)
+        {
+            this._nombre = nom;
+            this._apellido = ape;
+            this._edad = edad;
+            this._dni = dni;
+        }
+
+        ~Persona()
+        {
+            Console.WriteLine("Se ha eliminado la Persona");
+        }
+
+        public string Nombre
+        {
+            get => _nombre;
+            set => _nombre = value;
+ 
+        }
+
+        public string Apellido
+        {
+            get => _apellido; 
+            set => _apellido = value;
+
+        }
+
+        public int Edad
+        {
+            get => _edad;
+            set => _edad = value;
+        }
+
+        public int DNI
+        {
+            get => _dni;
+            set => _dni = value;
+        }
+
+        public void GetFullName()
+        {
+            Console.WriteLine(this._nombre + " " + this._apellido);
+        }
+
+        public void GetAge()
+        {
+            Console.WriteLine(this._edad.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using LabClases4.Clases;
+using System;
+
+namespace LabClases4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Pepe", "Perez", 24, 32552465);
+
+            p.GetFullName();
+            p.GetAge();
+        }
+    }
+}
